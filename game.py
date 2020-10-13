39584937251aa25e5260f1d95b130c6a5841bbc7
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-10-13 13:05:14 -0700

    Update game.py

--- game.py
+++ game.py
@@ -6,10 +6,42 @@ Rock Paper Scissors Game!
 Your documentation should go here
 """
 
+import os
+import tkinter as tk
+
 def main():
-  pass
+  def text1():
+    titleScreen["text"] = "COOL BEANS"
+    openTutorial()
 
+  def openTutorial():
+    tutorial.pack()
+    titleScreen.label.pack_forget()
+  def closeTutorial():
+    tutorial.widget.pack_forget()
 
+  main = tk.Tk()
+  main.geometry("500x500")
+  main.title("Rock Paper Scissors")
+  titleScreen = tk.Label(
+    text="Welcome to Rock Paper Scissors",
+  )
+  titleButton = tk.Button(
+    text = "START",
+    command=text1
+  )
+  tutorialButton = tk.Button(
+    text = "TUTORIAL",
+    command=openTutorial
+  )
+  tutorial = tk.Label(
+    text="Do you really not know how to play rock paper scissors?"
+  )
+
+  titleScreen.pack()
+  titleButton.pack()
+  tutorialButton.pack()
+  main.mainloop()
 
 
 main()
